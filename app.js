1dfae000df4848d679d0cbc7d1be54388d86595d
TopDocDev <55025255+TopDocDev@users.noreply.github.com>
2020-07-11 10:40:40 +0200

    getDb refactored + dynamic appointments

--- app.js
+++ app.js
@@ -6,129 +6,25 @@ const express     = require("express"),
     mongoose    = require("mongoose"),
     doc         = require("./models/doc"),
     Comment     = require("./models/comment"),
+    week        = require("./models/week"),
     seedDB      = require("./seeds"),
     sql         = require("mssql"),
     sequelize   = require("sequelize"),
     moment      = require('moment'),
-    custom      = require("./functions/custom"),
     axios       = require("axios"),
     async       = require("async")
 
+const custom = require("./functions/custom")
 
 
 
-mongoose.connect("mongodb://localhost/Doc"); 
+mongoose.connect("mongodb+srv://louis:louis@cluster0-bbdc4.mongodb.net/TopDoc?retryWrites=true&w=majority", {useNewUrlParser: true,  useUnifiedTopology: true }); 
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
 app.set("view engine", "ejs");
 app.use(express.static(__dirname + "/public"));
-//seedDB();
-var sampleDb = new sequelize(
-    'SampleDb', 'louis', 'password', {
-  host: 'localhost',
-  dialect: 'mssql',
-  port: 1433,
-  logging: false,
-});
 
-var User = sampleDb.define("user", {
-    firstName: sequelize.STRING,
-    lastName: sequelize.STRING,
-
-});
-let dbConfig = {  
-    server: 'localhost', 
-    database: "v21db",
-    authentication: {
-        type: 'default',
-        options: {
-            userName: 'louis', 
-            password: 'password'  
-        }
-    },
-}; 
-
-// connect to mssql database
-
-function getDb(req, res) {
-    var conn = new sql.ConnectionPool(dbConfig);  // create sql instance
-    var req = new sql.Request(conn);
-    conn.connect(function (err){
-        if (err) {
-            console.log(err);
-            return;
-        }
-        let a = {
-            number: 10000,
-            date: "01/01/2020",
-        }
-        req.query("SELECT TOP "+ a.number + "TT_PNAME as name, TT_DATAUS as 'end', DATEADD(MINUTE, -TT_DAUER, TT_DATAUS) AS start, 'green' as color from v21db.dbo.Tabelle1$ where TT_DATAUS > '" + a.date +"' and TT_PNR is not null order by TT_DATAUS", function (err, myobject){
-            if(err){
-                console.log(err);
-            } else {
-                doc.find({}, function(err, allDocs){
-                    if(err){
-                        console.log(err);
-                    } else {
-                       let arr = JSON.parse(JSON.stringify(myobject.recordset))
-                       let a = custom.makeArray(arr)
-                       let b = custom.getFiveDays(a)         
-                        res.render("docs/index",{
-                            doc : JSON.stringify(allDocs),
-                            data: JSON.stringify(b)
-                            })
-                    }
-                 });
-                //res.send(employees);
-            }
-            conn.close();
-        });
-    });
-}
-
-function getCalendar(req, res) {
-    var conn = new sql.ConnectionPool(dbConfig);  // create sql instance
-    var req = new sql.Request(conn);
-    conn.connect(function (err){
-        if (err) {
-            console.log(err);
-            return;
-        }
-        let a = {
-            number: 10000,
-            date: "01/01/2020",
-        }
-        req.query("SELECT TOP "+ a.number + "TT_PNAME as name, TT_DATAUS as 'end', DATEADD(MINUTE, -TT_DAUER, TT_DATAUS) AS start, 'green' as color from v21db.dbo.Tabelle1$ where TT_DATAUS > '" + a.date +"' and TT_PNR is not null order by TT_DATAUS", function (err, myobject){
-            if(err){
-                console.log(err);
-            } else {
-                doc.find({}, function(err, allDocs){
-                    if(err){
-                        console.log(err);
-                    } else {
-                        let arr = JSON.parse(JSON.stringify(myobject.recordset));
-                        let newArr = arr.map(e => ({
-                            name: e.name,
-                            start: moment(e.start).format("YYYY-MM-DD HH:mm"),
-                            end: moment(e.end).format("YYYY-MM-DD HH:mm"),
-                            color: "yellow",
-                            duration: moment.duration(moment(e.end).diff(moment(e.start))).asMinutes(),
-                            open: false,               
-                        }))
-                        // let split = custom.makeArray(newArr)
-                        res.render("calendar",{
-                            doc : JSON.stringify(allDocs),
-                            data: JSON.stringify(newArr)
-                            })
-                        
-                    }
-                 });
-                //res.send(employees);
-            }
-            conn.close();
-        });
-    });
-}
+seedDB();
 
 //ROUTES
 app.post("/login", function(req, res){
@@ -137,13 +33,50 @@ app.post("/login", function(req, res){
 
 app.get("/", (req, res) => res.render("landing"))
 
-app.post("")
-//INDEX
-app.get("/docs", (req, res) => getDb(req, res))
 
+
+
+app.post("/calendar", (req, res) => {
+    const weekArray = JSON.parse(req.body.params.week)
+    for (let i = 0; i < weekArray.length; i++) {
+        const e = weekArray[i];
+            week.create(e)  
+    }
+    console.log(weekArray)
+})
+//INDEX
+app.get("/docs", (req, res) => doc.find({}, function(err, allDocs){
+    if(err){
+        console.log(err)
+    } else {
+        week.find({}, function(err, allWeeks){
+            if(err){
+                console.log(err)
+            } else {
+                const sorted = allWeeks.sort(function(a, b){
+                    var x = a.end;
+                    var y = b.end;
+                    if (x < y) {return -1;}
+                    if (x > y) {return 1;}
+                    return 0;
+                  })
+                const data = JSON.stringify(custom.getFiveDays(sorted))
+                res.render("docs/index",{
+                    doc : JSON.stringify(allDocs),
+                    data: data
+                })
+                //console.log(data)
+            }
+        })
+    }
+}))
+try {
+    doc.deleteMany( { "name" : "Dr. Jasmine Parambia" } );
+ } catch (e) {
+    print (e);
+ }
 //CREATE
 app.post("/docs", function(req, res){
-
     var name = req.body.name;
     var image = req.body.image;
     var desc = req.body.description;
@@ -189,10 +122,9 @@ app.get("/docs/:id/signup", function(req, res){
 });
 */
 
-// Arztbereich
-
 app.get("/docs/buchung", (req, res) => res.render("docs/buchung"))
-app.get("/calendar", (req, res) => getCalendar(req,res))
+app.get("/calendar", (req, res) => custom.getCalendar(req,res))
+
 // ====================
 // COMMENTS ROUTES
 // ====================
@@ -231,4 +163,4 @@ var port = process.env.PORT || 3000;
 app.listen(port, function () {
   console.log("Server Has Started!");
 
-});
+})
